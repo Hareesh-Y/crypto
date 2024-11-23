53ca03cd3ad71d54fb946239054ff2782b7983ed
Hareesh-Y <155472037+Hareesh-Y@users.noreply.github.com>
2024-11-23 14:35:16 +0530

    Revert "Add two-factor authentication API functionality"

--- src/main/java/com/cryp/harsh/repository/TwoFactorOtpRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cryp.harsh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cryp.harsh.model.TwoFactorOtp;
-
-public interface TwoFactorOtpRepository extends JpaRepository<TwoFactorOtp , String>{
-	TwoFactorOtp findByUserId(Long userId);
-}
